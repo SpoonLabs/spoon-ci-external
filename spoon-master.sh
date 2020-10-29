2efaaa7858f4e66d8e6f935cc607b8192744b6c3
Martin Monperrus <martin.monperrus@gnieh.org>
2020-10-29 04:00:29 +0100

    add spoon-pull-request.sh

---
+++ spoon-master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# additional CI jobs for Spoon Master
+
+set -e
+
+echo "more stuff for $TRAVIS_BRANCH"
+
+# add it here
---
+++ spoon-pull-request.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# additional CI jobs for Spoon Pull Requests
+
+set -e
+
+echo "more stuff for https://github.com/INRIA/spoon/pull/$TRAVIS_PULL_REQUEST"
+
+# add it here
